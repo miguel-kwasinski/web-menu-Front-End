b8efb11d7e4ec76a324242cf10806bdf3bdbfdc0
Miguel-Kwasinski <mvkwasinski@gmail.com>
2024-10-16 16:55:40 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const data = [];
+  return (
+      <div className="Container">
+        <h1>Menu</h1>
+        <div className="Card-Grid">
+
+        </div>
+      </div>
+  )
+}
+
+export default App
